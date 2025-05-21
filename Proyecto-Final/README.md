033d0152a3f0a71abf63a06f95ea9552129ea178
José Ramón Blanco Espinoza <blanco.r.jose@hotmail.com>
2025-05-21 12:03:51 -0700

    Export de result CSV

--- Proyecto-Final/README.md
+++ Proyecto-Final/README.md
@@ -1,2 +1,17 @@
 # Hola mundo
 # Hola Maverick
+
+// Contenido del documento de proyecto final
+// 1. Portada
+// 2. Indice
+// 3. Introduccion
+// 4. Marco teorico de los algoritmos
+// 5. Implementación (Que herramientas usaron y porque (en este caso spark con scala))
+// 6. Resultados (Un tabular con los datos de 30 corridas por cada algoritmo y hacer un promedio 
+//                para ver su preformance)
+//    y su respectiva explicacion.
+// 7. Conclusiones
+// 8. Referencias (No wikipedia por ningun motivo, traten que sean de articulos cientificos)
+//    El documento debe estar referenciado 
+
+// Nota: si el documento no es presentado , no revisare su desarrollo del proyecto
--- Proyecto-Final/proyecto.scala
+++ Proyecto-Final/proyecto.scala
@@ -2,25 +2,9 @@
 //1.- Objectivo: Comparación del rendimiento de los siguientes algoritmos de machine learning
 // - SVM
 // - Decision Three
-// - Logistic Regresion
-// - Multilayer perceptron
+// - Logistic Regresion ✅
+// - Multilayer perceptron ✅
 //Con el siguiente data set: https://archive.ics.uci.edu/ml/datasets/Bank+Marketing
-
-// Contenido del documento de proyecto final
-// 1. Portada
-// 2. Indice
-// 3. Introduccion
-// 4. Marco teorico de los algoritmos
-// 5. Implementación (Que herramientas usaron y porque (en este caso spark con scala))
-// 6. Resultados (Un tabular con los datos de 30 corridas por cada algoritmo y hacer un promedio 
-//                para ver su preformance)
-//    y su respectiva explicacion.
-// 7. Conclusiones
-// 8. Referencias (No wikipedia por ningun motivo, traten que sean de articulos cientificos)
-//    El documento debe estar referenciado 
-
-// Nota: si el documento no es presentado , no revisare su desarrollo del proyecto
-
 
 // Import
 println(" ")
@@ -196,8 +180,12 @@ for( i <- 1 to iteraciones ){
 //finalSeq.foreach(println)
 
 println(s"******** Resultados de las ejecuciones ********")
-val resultadosDF = spark.sparkContext.parallelize(resultados).toDF("Iteración","Semilla","MLP Accuracy","MLP Duración (ms)","LR Accuracy", "LR Duración (ms)")
-resultadosDF.show()
+val resultadosDF = spark.sparkContext.parallelize(resultados).toDF("Iteracion","Semilla","MLP Accuracy","MLP Duracion (ms)","LR Accuracy", "LR Duracion (ms)")
+resultadosDF.show(iteraciones)
 
 println(s"******** Resumen estadístico de los resultados ********")
 resultadosDF.describe().show()
+
+println(s"Exportando resultados a CSV")
+resultadosDF.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").option("encoding","UTF-8").mode("overwrite").save("resultados-iteraciones")
+resultadosDF.describe().coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").option("encoding","UTF-8").mode("overwrite").save("resultados-describe")
